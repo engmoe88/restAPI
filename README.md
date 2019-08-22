b4d5ad15c4b9902fb6772e27569267c1c4abb9e7
Mohammed Almukhtar <msalmukhtar@sbgtv.com>
2019-08-21 18:57:22 -0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # restAPI
+
+/NOTE IMPORTANT: TO run the app, you NEED FIRST to run <mongod --config /usr/local/etc/mongod.conf> to let mongodb run then you can use your npm run moStart script
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-//NOTE IMPORTANT: TO run the app, you NEED FIRST to run <mongod --config /usr/local/etc/mongod.conf> to let mongodb run then you can use your npm run moStart script
+
 
 require('dotenv').config();
 
